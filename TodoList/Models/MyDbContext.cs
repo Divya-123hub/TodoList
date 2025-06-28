30c1a3534d6c7f03a5c483ff5c1db598473ddbea
Divya-123hub <divyanaddi@gmail.com>
2025-06-28 12:51:20 +0530

    Add project files.

---
+++ TodoList/Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoList.Models
+{
+    public class MyDbContext:DbContext
+    {
+        public DbSet<Person> People {  get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+
+
+        }
+    }
+}
